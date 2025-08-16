82c413cd17ddb564992b818d1a4954331804ef03
BiswajitBeheraDev <beherabiswajit344@gmail.com>
2025-08-16 09:55:05 +0530

    update new ...slug

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -1,10 +1,11 @@
-import { Suspense } from "react";
-import NotFoundContent from "./Myclientcomponet";
+export default async function NotFoundPage({ searchParams }) {
+  const params = await searchParams; // 
+  const errorCode = params?.error;
 
-export default function NotFoundPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <NotFoundContent/>
-    </Suspense>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Page Not Found</h1>
+      {errorCode && <p>Error Code: {errorCode}</p>}
+    </div>
   );
 }
